03c2a37ed191520398dc1d0bd8a738d7d56b63bb
itai <itai.avrahami@sysaid.com>
2016-08-11 12:10:20 +0300

    add hello

--- src/test/java/server/installer/InstallSanitySuite.java
+++ src/test/java/server/installer/InstallSanitySuite.java
@@ -46,6 +46,7 @@ public class InstallSanitySuite extends AbstractSuite{
 	
 	//@Test
 	/*public void calc_Demo(){
+	 * hello
 		LogManager.info("Run Calc..");
 		validate.validate(true, "Calc Validate - true");
 		AutoItAPI.run("calc.exe");

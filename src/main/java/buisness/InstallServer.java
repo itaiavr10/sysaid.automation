be699944cec4441dd3701b6a3d231066252df7cd
itai <itai.avrahami@sysaid.com>
2016-08-16 14:45:41 +0300

    installer verification - improve

--- src/main/java/buisness/InstallServer.java
+++ src/main/java/buisness/InstallServer.java
@@ -10,8 +10,20 @@ public class InstallServer {
 	
 	
 	public static void validateInstallation(){
+		
+		validateSysAidFiles();
+		
+		//validate SysAidAgent & SysAidServer services are running
+		SystemUtils.validateService("SysAid Agent", true); //TODO : Should be an enum
+		SystemUtils.validateService("SysAid Server", true); //TODO : Should be an enum
+		
+		//validate icons on desktop:
+		SystemUtils.validateFileExist("SysAid Login.lnk", SystemUtils.getPublicDesktopPath(), true); //TODO : Should be an enum
+		SystemUtils.validateFileExist("SysAid.lnk", SystemUtils.getPublicDesktopPath(), true); //TODO : Should be an enum
+				
+				
 		//wait for process to finish installstion
-		SystemUtils.waitForProcessStop("SA.exe", 40 * 1000 , 3000);
+		SystemUtils.waitForProcessStop("SA.exe", 60 * 1000 , 3000);
 		//validate SA.exe is finished to run
 		SystemUtils.validateProcess("SA.exe", false); //TODO : Should be an enum
 		//validate SA Process start to run
@@ -19,14 +31,9 @@ public class InstallServer {
 		SystemUtils.validateProcess("SysAidSM.exe", true); //TODO : Should be an enum
 		SystemUtils.validateProcess("SysAidWorker.exe", true); //TODO : Should be an enum
 	
-		//validate SysAidAgent & SysAidServer services are running
-		SystemUtils.validateService("SysAid Agent", true); //TODO : Should be an enum
-		SystemUtils.validateService("SysAid Server", true); //TODO : Should be an enum
-		//validate icons on desktop:
-		SystemUtils.validateFileExist("SysAid Login.lnk", SystemUtils.getPublicDesktopPath(), true); //TODO : Should be an enum
-		SystemUtils.validateFileExist("SysAid.lnk", SystemUtils.getPublicDesktopPath(), true); //TODO : Should be an enum
 		
-		validateSysAidFiles();
+		
+		
 	}
 	
 	

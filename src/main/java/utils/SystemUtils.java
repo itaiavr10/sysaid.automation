5af852da5e635e778c6820bd406ea3ef8fc574d0
itai <itai.avrahami@sysaid.com>
2016-08-18 16:45:35 +0300

    FileUtils - replace line

--- src/main/java/utils/SystemUtils.java
+++ src/main/java/utils/SystemUtils.java
@@ -1,9 +1,12 @@
 package utils;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -18,16 +21,15 @@ import org.apache.logging.log4j.util.Strings;
 
 import base.LogManager;
 import base.TestManager;
+
 //OSUtils
 public class SystemUtils {
-	
-	
-	
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//												Files														   //
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	
-	public static class Files{
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//												Files														   //
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	public static class Files {
 
 		//TODO: Check
 		public static String read(final File srcFile) throws IOException {
@@ -35,85 +37,80 @@ public class SystemUtils {
 			input = FileUtils.readFileToString(srcFile);
 			return input;
 		}
-		
-		
-		public static void scan(String filePath, String... searchQuery){
+
+		public static void scan(String filePath, String... searchQuery) {
 			LogManager.info("Scan File: " + filePath);
-		    BufferedReader br = null;
-		     try{
-		            br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
-		            String line;
-		            while ((line = br.readLine()) != null){
-		                 for (String reg : searchQuery) {
-		                 	 if (line.toLowerCase().contains(reg.trim().toLowerCase()))
-		                 		 LogManager.error("Found Line: " + line);
-		 				}
-		            }
-		        }catch (Exception e) {
-		        	LogManager.error("Scan File - Error : " + e.getMessage());
-				}
-		        finally{
-		            try{
-		                if (br != null)
-		                    br.close();
-		            }
-		            catch (Exception e){ }
-		        }
-	        /*Scanner scanner = null;
-	        try{
-	            scanner = new Scanner(new File(filePath));
-	            while (scanner.hasNextLine()){
-	                String line = scanner.nextLine();
-	                for (String reg : searchQuery) {
-	                	 if (line.toLowerCase().contains(reg.trim().toLowerCase()))
-	                		 LogManager.error("Found Line: " + line);
+			BufferedReader br = null;
+			try {
+				br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
+				String line;
+				while ((line = br.readLine()) != null) {
+					for (String reg : searchQuery) {
+						if (line.toLowerCase().contains(reg.trim().toLowerCase()))
+							LogManager.error("Found Line: " + line);
 					}
-	            }
-	        } catch (FileNotFoundException e) {
-	        	LogManager.error("Scan File - Error : " + e.getMessage());
+				}
+			} catch (Exception e) {
+				LogManager.error("Scan File - Error : " + e.getMessage());
+			} finally {
+				try {
+					if (br != null)
+						br.close();
+				} catch (Exception e) {
+				}
 			}
-	        finally{
-	            try{
-	                if (scanner != null)
-	                    scanner.close();
-	            }
-	            catch (Exception e) {}
-	        }*/
-	    }
-		
-		
+			/*Scanner scanner = null;
+			try{
+			    scanner = new Scanner(new File(filePath));
+			    while (scanner.hasNextLine()){
+			        String line = scanner.nextLine();
+			        for (String reg : searchQuery) {
+			        	 if (line.toLowerCase().contains(reg.trim().toLowerCase()))
+			        		 LogManager.error("Found Line: " + line);
+					}
+			    }
+			} catch (FileNotFoundException e) {
+				LogManager.error("Scan File - Error : " + e.getMessage());
+			}
+			finally{
+			    try{
+			        if (scanner != null)
+			            scanner.close();
+			    }
+			    catch (Exception e) {}
+			}*/
+		}
+
 		public static String getProjectPath() {
 			return System.getProperty("user.dir");
 		}
-		
+
 		public static String getPublicDesktopPath() {
 			//return String.format("C:\\Users\\%s\\Desktop", System.getProperty("user.name"));
 			return String.format("C:\\Users\\Public\\Desktop");
 		}
-		
+
 		public static String getResourcesDirectoryPath() {
 			return getProjectPath() + "\\src\\main\\resources";
 		}
-		
-		
-		public static void createFile(String filePath , final String fileContent) throws IOException {
+
+		public static void createFile(String filePath, final String fileContent) throws IOException {
 			File newFile = new File(filePath);
 			FileUtils.write(newFile, fileContent);
 		}
-		
-		
-		public static void validateExist(String fileName , String direcoryPath , boolean shouldExist){
-			validateExist(direcoryPath + "\\" + fileName , shouldExist , null);
+
+		public static void validateExist(String fileName, String direcoryPath, boolean shouldExist) {
+			validateExist(direcoryPath + "\\" + fileName, shouldExist, null);
 		}
-		
-		public static void validateExist(String filePath ,  final boolean shouldExist){
-			validateExist(filePath ,  shouldExist , 5000);
+
+		public static void validateExist(String filePath, final boolean shouldExist) {
+			validateExist(filePath, shouldExist, 5000);
 		}
-		
-		public static void validateExist(String filePath ,  final boolean shouldExist , Integer maxTimeOutMs){
+
+		public static void validateExist(String filePath, final boolean shouldExist, Integer maxTimeOutMs) {
 			final ValueRef<Boolean> isExist = new ValueRef<Boolean>(false);
 			final File file = new File(filePath);
-			boolean ispass = Utils.tryUntil(new ActionWrapper("Validat File Exist : " + filePath , maxTimeOutMs) {
+			boolean ispass = Utils.tryUntil(new ActionWrapper("Validat File Exist : " + filePath, maxTimeOutMs) {
 				@Override
 				public boolean invoke() throws Exception {
 					boolean results = file.exists();
@@ -122,7 +119,7 @@ public class SystemUtils {
 				}
 			});
 			TestManager.validator().validate(ispass, String.format("Validate File Exist : %s . Expected = %s , Actual = %s", filePath, shouldExist, isExist.value));
-			
+
 			/*File file = new File(filePath);
 			boolean isExist = file.exists();
 			TestManager.validator().validate(shouldExist == isExist, String.format("Validate File Exist : %s . Expected = %s , Actual = %s",filePath, shouldExist, isExist));*/
@@ -145,18 +142,63 @@ public class SystemUtils {
 			}
 			TestManager.validator().validate(isok, String.format("Validate File Exist : %s . Expected = %s , Actual = %s",filePath, shouldExist, isok));*/
 		}
-	
-	
+
+		public static void replaceLine(String filePath, String originalLine, String newLine) {
+			LogManager.debug(String.format("Replace File Content, Original line= %s , new line=%s",originalLine,newLine));
+			String tmpFileName = "tmp.dat";
+			boolean lineFound = false;
+			BufferedReader br = null;
+			BufferedWriter bw = null;
+			try {
+				br = new BufferedReader(new FileReader(filePath));
+				bw = new BufferedWriter(new FileWriter(tmpFileName));
+				String line;
+				while ((line = br.readLine()) != null) {
+					if (line.contains(originalLine)){
+						lineFound=true;
+						line = newLine;
+					}
+					bw.write(line + "\n");
+				}
+				if(!lineFound){
+					throw new Exception("Failed to find line");
+				}
+			} catch (Exception e) {
+				LogManager.error("Replace File Content- Error : " + e.getMessage());
+			} finally {
+				try {
+					if (br != null)
+						br.close();
+				} catch (IOException e) {}
+				try {
+					if (bw != null)
+						bw.close();
+				} catch (IOException e) {}
+			}
+			if(lineFound){
+				try {
+					FileUtils.forceDelete(new File(filePath));
+					moveFile(tmpFileName , filePath);
+				} catch (IOException e) {
+					LogManager.error("Replace File Content- Error : " + e.getMessage());
+				}
+			}
+			
+		}
+		
+		public static void moveFile(String srcPath,String destPath) throws IOException {
+			FileUtils.moveFile(new File(srcPath), new File(destPath));
+		}
+
 	}
-	
-	
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//												Process														   //
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	
-	public static class Processes{
-	
-		public static synchronized void validate(final String processName , final boolean shouldRun){
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//												Process														   //
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	public static class Processes {
+
+		public static synchronized void validate(final String processName, final boolean shouldRun) {
 			final ValueRef<Boolean> isRun = new ValueRef<Boolean>(false);
 			boolean pass = Utils.tryUntil(new ActionWrapper("Validate Process : " + processName) {
 				@Override
@@ -166,15 +208,15 @@ public class SystemUtils {
 					return results == shouldRun;
 				}
 			});
-			TestManager.validator().validate(pass, String.format("Validate Process : %s . Expected = %s , Actual = %s",processName, shouldRun, isRun.value));
-			
+			TestManager.validator().validate(pass, String.format("Validate Process : %s . Expected = %s , Actual = %s", processName, shouldRun, isRun.value));
+
 			/*boolean isRun = isProcessRunning(processName);
 			TestManager.validator().validate(shouldRun == isRun, String.format("Validate Process : %s . Expected = %s , Actual = %s",processName, shouldRun, isRun));*/
 		}
-		
-		public static void waitForProcessStop(final String processName, int maxTimeoutInMiliSec , int waitInterval) {
+
+		public static void waitForProcessStop(final String processName, int maxTimeoutInMiliSec, int waitInterval) {
 			final ValueRef<Boolean> isStoped = new ValueRef<Boolean>(false);
-			boolean pass  = Utils.tryUntil(new ActionWrapper("Wait For Process Stop: " + processName , maxTimeoutInMiliSec , waitInterval) {
+			boolean pass = Utils.tryUntil(new ActionWrapper("Wait For Process Stop: " + processName, maxTimeoutInMiliSec, waitInterval) {
 				@Override
 				public boolean invoke() throws Exception {
 					boolean results = !isProcessRunning(processName);
@@ -182,11 +224,11 @@ public class SystemUtils {
 					return results;
 				}
 			});
-			
-			if(!pass){
+
+			if (!pass) {
 				LogManager.error("Wait For Process Stop - Failed ");
 			}
-			
+
 			/*
 			
 			LogManager.debug("Wait For Process Stop: " + processName);
@@ -206,14 +248,14 @@ public class SystemUtils {
 				LogManager.error("Wait For Process Stop - Failed ");
 			}*/
 		}
-		
-		public static boolean isProcessRunning(String processName){
+
+		public static boolean isProcessRunning(String processName) {
 			List<String> processList = getProcessDetails(processName);
-			if(processList == null || processList.isEmpty())
+			if (processList == null || processList.isEmpty())
 				return false;
 			return true;
 		}
-	
+
 		private static List<String> getProcessDetails(String processName) {
 			LogManager.debug("Get Process Details for process: " + processName);
 			List<String> processList = null;
@@ -221,31 +263,30 @@ public class SystemUtils {
 			try {
 				String cmd = String.format("tasklist.exe /fi \"Imagename eq %s\"", processName);
 				Process proc = Runtime.getRuntime().exec(cmd);
-				InputStream procOutput = proc.getInputStream ();
-				
-					input =new BufferedReader(new InputStreamReader(procOutput));
-					String line;
-					
-					while ((line = input.readLine()) != null) {
-						if(line.contains("No tasks")){
-							LogManager.debug("tasklist.exe - No results for :" + processName);
-							return null;
-						}
-						if(line.contains("====="))
-							break;
-				    }
-					//Start reading table results
-					processList = new ArrayList<String>();
-					LogManager.debug("Found Processes :");
-					while ((line = input.readLine()) != null) {
-						LogManager.debug(line); //<-- Parse data here.
-				        processList.add(line);
-				    }
-				    
-				    
+				InputStream procOutput = proc.getInputStream();
+
+				input = new BufferedReader(new InputStreamReader(procOutput));
+				String line;
+
+				while ((line = input.readLine()) != null) {
+					if (line.contains("No tasks")) {
+						LogManager.debug("tasklist.exe - No results for :" + processName);
+						return null;
+					}
+					if (line.contains("====="))
+						break;
+				}
+				//Start reading table results
+				processList = new ArrayList<String>();
+				LogManager.debug("Found Processes :");
+				while ((line = input.readLine()) != null) {
+					LogManager.debug(line); //<-- Parse data here.
+					processList.add(line);
+				}
+
 			} catch (Exception e) {
 				TestManager.validator().assertFalse("Error on  getProcessDetails utils : " + e.getMessage());
-			}finally{
+			} finally {
 				try {
 					input.close();
 				} catch (IOException e) {
@@ -254,19 +295,18 @@ public class SystemUtils {
 			return processList;
 		}
 	}
-	
-	
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//											Services														   //
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	
-	public static class Services{
-	
-		public static void validate(String serviceName , boolean shouldRun){
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//											Services														   //
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	public static class Services {
+
+		public static void validate(String serviceName, boolean shouldRun) {
 			boolean isRun = isServiceRunning(serviceName);
-			TestManager.validator().validate(shouldRun == isRun, String.format("Validate Service is running : %s . Expected = %s , Actual = %s",serviceName, shouldRun, isRun));
+			TestManager.validator().validate(shouldRun == isRun, String.format("Validate Service is running : %s . Expected = %s , Actual = %s", serviceName, shouldRun, isRun));
 		}
-		
+
 		private static boolean isServiceRunning(String serviceName) {
 			BufferedReader input = null;
 			try {
@@ -274,18 +314,18 @@ public class SystemUtils {
 				//String cmd = String.format("net START | find \"%s\"", serviceName);
 				String cmd = "net START";
 				Process proc = Runtime.getRuntime().exec(cmd);
-				InputStream procOutput = proc.getInputStream ();
-				
-					input =new BufferedReader(new InputStreamReader(procOutput));
-					String line;
-					while ((line = input.readLine()) != null) {
-						if(line.contains(serviceName))
-							return true;
-						//return false;
-				    }
+				InputStream procOutput = proc.getInputStream();
+
+				input = new BufferedReader(new InputStreamReader(procOutput));
+				String line;
+				while ((line = input.readLine()) != null) {
+					if (line.contains(serviceName))
+						return true;
+					//return false;
+				}
 			} catch (Exception e) {
 				TestManager.validator().assertFalse("Error on  getProcessDetails utils : " + e.getMessage());
-			}finally{
+			} finally {
 				try {
 					input.close();
 				} catch (IOException e) {
@@ -293,7 +333,7 @@ public class SystemUtils {
 			}
 			return false;
 		}
-		
+
 		/*public static boolean isServiceRunning(String serviceName) {
 			LogManager.debug("Is Service Running for service: " + serviceName);
 			try {
--- src/test/java/itai/Suite1.java
+++ src/test/java/itai/Suite1.java
@@ -12,7 +12,8 @@ public class Suite1 extends AbstractSuite{
 	@Test
 	public void A(){
 		//XmlUtils.validteNodeValue("C:\\AgentConfigurationFile.xml", "FirstTime", "Y", 120000, 5000);
-		SystemUtils.Files.scan("C:\\SysAidAgentLog.txt", "Error","Exception");
+		//SystemUtils.Files.scan("C:\\SysAidAgentLog.txt", "Error","Exception");
+		SystemUtils.Files.replaceLine("C:\\log4j.properties", "log4j.logger.com.ilient=INFO, sysaidLogFile", "log4j.logger.com.ilient=DEBUG, sysaidLogFile");
 		
 	}
 
